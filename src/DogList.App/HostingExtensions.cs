485af464ea5079e629752cca025a3a7aeb504cbe
Yevhenii Nabok <1337niceshot@gmail.com>
2024-10-26 14:33:29 +0300

    config controllers

--- src/DogList.App/HostingExtensions.cs
+++ src/DogList.App/HostingExtensions.cs
@@ -4,6 +4,9 @@ public static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
+        builder.Services.AddControllers()
+            .AddApplicationPart(Presentation.AssemblyReference.Assembly);
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
@@ -17,6 +20,8 @@ public static class HostingExtensions
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        app.MapControllers();
 
         app.Run();
 
