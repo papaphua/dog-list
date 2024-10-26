7962e0f19b90d9595ba37379e6460b71db42ecbf
Yevhenii Nabok <1337niceshot@gmail.com>
2024-10-26 14:30:21 +0300

    add swagger

--- src/DogList.App/HostingExtensions.cs
+++ src/DogList.App/HostingExtensions.cs
@@ -4,11 +4,20 @@ public static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
         return builder.Build();
     }
 
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
         app.Run();
 
         return app;
